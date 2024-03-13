1cec67c355b9c008eb0703f96d6d952a41f79ae9
DevPentakill <fmahdi.bd@gmail.com>
2024-03-14 01:39:53 +0600

    created, connected to daisy and made initiaL FETCH

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Creating a function that will fetch data from PH server
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones);
+}
+
+loadPhone();
