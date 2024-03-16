327091fdfd7890b7318914502faf4e240ac1d4e8
DevPentakill <fmahdi.bd@gmail.com>
2024-03-16 15:47:51 +0600

    created seearch functionality

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
+
 //Creating a function that will fetch data from PH server
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
-    //putting all the phones information in a variable called phones
-    const phones = data.data;
-    // console.log(phones);
-    //passing that variable to the display method, this will
-    displayPhones(phones);
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+  const data = await res.json();
+  //putting all the phones information in a variable called phones
+  const phones = data.data;
+  // console.log(phones);
+  //passing that variable to the display method, this will
+  displayPhones(phones);
 }
-//calling the loadphone function with every time website reload/load
-loadPhone();
 
-const displayPhones = (phones) =>{
+const displayPhones = (phones) => {
 
-    //Step 1 using dom to catch the empty container of HTML
-    const phoneContainer = document.getElementById('phone-container');
-    // console.log(phones);
-    phones.forEach((phone,index) => {
-        console.log(phone);
-        // Step 2 create empty card
-        const phoneCard = document.createElement('div');
-        phoneCard.classList = "card w-72 bg-green-100 shadow-xl p-3 m-3";
+  //Step 1 using dom to catch the empty container of HTML
+  const phoneContainer = document.getElementById('phone-container');
+  //clearing all the previosuly loaded phones
+  phoneContainer.textContent = '';
+  // console.log(phones);
+  phones.forEach((phone, index) => {
+    console.log(phone);
+    // Step 2 create empty card
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = "card w-72 bg-green-100 shadow-xl p-3 m-3";
 
-        //Step 3 setup card content with inner html
-        phoneCard.innerHTML = `
+    //Step 3 setup card content with inner html
+    phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="${phone.brand}" />
         </figure>
         <div class="card-body items-center">
@@ -37,9 +38,18 @@ const displayPhones = (phones) =>{
             <button class="btn btn-primary">Show Details</button>
           </div>
         </div>
-        `
-        //step 4 Appending it to the empty container in HTML
-        phoneContainer.appendChild(phoneCard);
+        `;
+    //step 4 Appending it to the empty container in HTML
+    phoneContainer.appendChild(phoneCard);
 
-    });
+  });
+}
+
+//handeling the search button
+const searchClick = () => {
+  const searchInput = document.getElementById('searchField');
+  const searchText = searchInput.value;
+  console.log(searchText);
+  loadPhone(searchText);
+  
 }
